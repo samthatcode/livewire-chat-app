20aa0c7a7aef500c08eec65c871223b646c4d347
JaiveerChavda <jayveersinhchavda555@gmail.com>
2025-02-07 19:41:07 +0530

    feat: set roomId as reactive property so it gets updated whenever user switch rooms.

--- app/Livewire/Chats/Create.php
+++ app/Livewire/Chats/Create.php
@@ -1,24 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire\Chats;
 
 use App\Models\Chat;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\Reactive;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+
 class Create extends Component
 {
-
-    public ?int $roomId;
+    #[Locked]
+    #[Reactive]
+    public ?int $roomId = null;
 
     #[Validate('required|string')]
-    public string $message; 
+    public string $message;
 
     public function create()
     {
         $this->validate();
 
         // create a new chat in the database
-
         Chat::factory()->create([
             'room_id' => $this->roomId,
             'user_id' => auth()->id(),
