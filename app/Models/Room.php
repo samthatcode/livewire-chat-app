5911b3f040ce7376e42195e859f0cab7109a74e0
Jaiveersinh Chavda <108678186+JaiveerChavda@users.noreply.github.com>
2025-02-08 18:50:48 +0530

    Update app/Models/Room.php
    punyapal suggestion , add array key and value
    
    Co-authored-by: Punyapal Shah  <53343069+MrPunyapal@users.noreply.github.com>

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Carbon;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property-read User $user
- * @property-read Collection|User[] $users
+ * @property-read Collection<array-key, User> $users
  * @property-read Chat[] $chats
  */
 class Room extends Model
