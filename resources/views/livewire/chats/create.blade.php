3087a97fe7d1d3ff7b03b5f35dcfd7954386487e
JaiveerChavda <jayveersinhchavda555@gmail.com>
2025-02-09 14:41:29 +0530

    refactor: use x-input component for input message field in chat create component

--- resources/views/livewire/chats/create.blade.php
+++ resources/views/livewire/chats/create.blade.php
@@ -1,17 +1,18 @@
+@php
+    $messageError = $errors->has('message') ? 'dark:border-2 dark:border-red-500' : 'border-gray-300';
+@endphp
 <div class="mt-4">
     <form wire:submit="create">
-        <div class="flex items-center gap-3">
-            <input type="text" 
-                wire:model="message"                
-                required
-                @class([
-                    'w-full rounded-lg px-4 py-2',
-                    'border-gray-300' => !$errors->has('message'),
-                    'border-2 border-red-500' => $errors->has('message')
-                ])
-
+        <div class="flex items-center gap-3">        
+            <x-text-input wire:model="message" 
+                id="message" 
+                name="message" 
+                type="text"  
+                required 
+                autofocus 
+                class="w-full rounded-lg px-4 py-2  {{ $messageError }}" 
             />
-            
+
             <button type="submit"
             class="text-white font-bold bg-blue-500 py-2 px-4 rounded disabled:cursor-not-allowed"
                 :class="$wire.message.length === 0 ? 'disabled:opacity-50' : '' "
