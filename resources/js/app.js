0cf186887b1848f5f9c224c421684f76f86a5083
Punyapal Shah <mrpunyapal@gmail.com>
2025-03-02 12:39:16 +0530

    feat: add notification system with Alpine.js and Livewire integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm'
+import { notifications, notify } from './notification.js'
+
+window.Alpine = Alpine
+
+Alpine.data('notifications', notifications)
+
+window.Alpine.plugin(notify)
+
+Livewire.start()
