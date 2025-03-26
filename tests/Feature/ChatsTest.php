2a8c9843dff8039f1c36c6321904700c15cc1205
Punyapal Shah <mrpunyapal@gmail.com>
2025-03-26 21:24:35 +0530

    test: update ChatsTest to assert create chat component visibility based on room selection

--- tests/Feature/ChatsTest.php
+++ tests/Feature/ChatsTest.php
@@ -2,20 +2,39 @@
 
 declare(strict_types=1);
 
-use App\Livewire\Chats\Index;
+use App\Livewire\Chats\Index as ChatsIndex;
+use App\Livewire\Chats\Create as CreateChat;
 use App\Livewire\Pages\Chats;
-use App\Livewire\Rooms\Create;
+use App\Livewire\Rooms\Create as CreateRoom;
 use App\Livewire\Rooms\Index as RoomsIndex;
+use App\Models\Room;
 use App\Models\User;
 
-test('Chats page is displayed', function () {
+test('chats page is displayed', function () {
     $user = User::factory()->create();
 
     $this->actingAs($user)
         ->get('/chats')
         ->assertSeeLivewire(Chats::class)
-        ->assertSeeLivewire(Index::class)
+        ->assertSeeLivewire(ChatsIndex::class)
         ->assertSeeLivewire(RoomsIndex::class)
-        ->assertSeeLivewire(Create::class)
+        ->assertSeeLivewire(CreateRoom::class)
+        ->assertDontSeeLivewire(CreateChat::class)
+        ->assertOk();
+});
+
+test('create chat component should be there if room is selected', function () : void {
+    $user = User::factory()->create();
+    $room = Room::factory()
+        ->hasAttached($user, relationship: 'users')
+        ->create();
+
+    $this->actingAs($user)
+        ->get('/chats?roomId=' . $room->id)
+        ->assertSeeLivewire(Chats::class)
+        ->assertSeeLivewire(ChatsIndex::class)
+        ->assertSeeLivewire(RoomsIndex::class)
+        ->assertSeeLivewire(CreateRoom::class)
+        ->assertSeeLivewire(CreateChat::class)
         ->assertOk();
 });
