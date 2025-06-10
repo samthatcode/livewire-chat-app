30b4bcedd88e0c1a6d13f28599fb2089e8391b09
Jaiveersinh Chavda <108678186+JaiveerChavda@users.noreply.github.com>
2025-06-10 22:57:09 +0530

    Update database/migrations/2025_06_01_152552_create_chat_user_favourites_table.php
    Co-authored-by: Punyapal Shah  <53343069+MrPunyapal@users.noreply.github.com>

--- database/migrations/2025_06_01_152552_create_chat_user_favourites_table.php
+++ database/migrations/2025_06_01_152552_create_chat_user_favourites_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chat_user_favourites');
+        Schema::dropIfExists('chat_user_favourite');
     }
 };
