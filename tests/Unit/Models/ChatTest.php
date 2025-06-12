4549e8eae64f07b00fad429549065242f332b905
JaiveerChavda <jayveersinhchavda555@gmail.com>
2025-06-12 22:05:14 +0530

    test: enhance Chat model tests for favouritedBy relationship

--- tests/Unit/Models/ChatTest.php
+++ tests/Unit/Models/ChatTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Models\Chat;
 use App\Models\Room;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 test('to array', function () {
     $chat = Chat::factory()->create()->fresh();
@@ -24,8 +25,13 @@ test('relationships', function () {
     $chat = Chat::factory()
         ->for(Chat::factory(), 'parent')
         ->create();
+    $chat->favouritedBy()->attach($chat->user->id);
+    $chat->favouritedBy()->attach(User::factory()->create()->id);
+
 
     expect($chat->user)->toBeInstanceOf(User::class);
     expect($chat->room)->toBeInstanceOf(Room::class);
     expect($chat->parent)->toBeInstanceOf(Chat::class);
+    expect($chat->favouritedBy)->toBeInstanceOf(Collection::class);
+    expect($chat->favouritedBy)->each()->toBeInstanceOf(User::class);
 });
